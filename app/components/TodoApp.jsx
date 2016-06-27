db75edc57f16426310ba64c7aefa1bd32db73ca4
Nate Kimball <nsktrombone84@hotmail.com>
2016-06-26 21:17:49 -0700

    refactor app to work with localStorage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -6,53 +6,9 @@ import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
 
-var TodoAPI = require('TodoAPI');
-
 var TodoApp = React.createClass({
 
-    getInitialState: function(){
-        return {
-            showCompleted: false,
-            searchText: '',
-            todos: TodoAPI.getTodos()
-        };
-    },
-
-    componentDidUpdate: function(){
-        TodoAPI.setTodos(this.state.todos);
-    },
-
-    handleAddTodo: function(text){
-        if(text){
-            this.setState({
-                todos: [
-                    ...this.state.todos,
-                    {
-                        id: uuid(),
-                        text: text,
-                        completed: false,
-                        createdAt: moment().unix(),
-                        completedAt: undefined
-                    }
-                ]
-            });
-        } else {
-            alert('Hey, man. Like, enter some text.');
-        }
-    },
-
-    handleSearch: function(showCompleted, searchText){
-        this.setState({
-            showCompleted: showCompleted,
-            searchText: searchText.toLowerCase()
-        });
-    },
-
     render: function(){
-
-        var {todos, showCompleted, searchText} = this.state;
-
-        var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
         return(
             <div>
@@ -60,9 +16,9 @@ var TodoApp = React.createClass({
                 <div className="row">
                     <div className="column small-11 medium-6 large-5 small-centered">
                         <div className="container">
-                            <TodoSearch onSearch={this.handleSearch} />
+                            <TodoSearch />
                                 <TodoList />
-                            <AddTodo onSubmitTodo={this.handleAddTodo} />
+                            <AddTodo />
                         </div>
                     </div>
                 </div>
