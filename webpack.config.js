6dcad476ecb1d9fa48db3cbb7a2187039fa62ce7
Nate Kimball <nsktrombone84@hotmail.com>
2016-06-28 22:15:19 -0700

    a

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         }
     }),
     new webpack.DefinePlugin({
-        'process.env.NODE_ENV': '"development"'
+        'process.env.NODE_ENV': '"production"'
     })
   ],
   output: {
