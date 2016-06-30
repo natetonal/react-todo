a54fdde113ed8e4ae9a2dbca321b503807885bbd
Nate Kimball <nsktrombone84@hotmail.com>
2016-06-29 22:31:28 -0700

    Attempting to fix issue with env files

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,15 +1,17 @@
 import firebase from 'firebase';
 
 try {
+    console.log(process.env.API_KEY);
     var config = {
-        apiKey: "AIzaSyA_LMe-xU-f4gjjbIg9OqhkZK55aKImL5I",
-        authDomain: "nates-todo-app.firebaseapp.com",
-        databaseURL: "https://nates-todo-app.firebaseio.com",
-        storageBucket: "nates-todo-app.appspot.com",
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        storageBucket: process.env.STORAGE_BUCKET
     };
+
     firebase.initializeApp(config);
 } catch (e) {
-
+    console.log('Error with Firebase: ', e);
 }
 
 export var firebaseRef = firebase.database().ref();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 var webpack = require('webpack');
 var path = require('path');
+var envFile = require('node-env-file');
 
-// process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+console.log('environment: ', process.env.NODE_ENV);
+
+
+try {
+    envFile(`./config/${process.env.NODE_ENV}.env`);
+} catch(e) {
+    console.log('error joining path for envFile.', e);
+}
 
 module.exports = {
   entry: [
@@ -23,7 +33,13 @@ module.exports = {
         }
     }),
     new webpack.DefinePlugin({
-        'process.env.NODE_ENV': '"production"'
+        'process.env': {
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            'API_KEY': JSON.stringify(process.env.API_KEY),
+            'AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+            'DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+            'STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET)
+        }
     })
   ],
   output: {
